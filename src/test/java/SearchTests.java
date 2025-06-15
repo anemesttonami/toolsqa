4b4376ebf48f16b1738e704db6839621ef4eeeea
Andrey Albaev <anemesttonami@gmail.com>
2025-06-15 16:15:02 +0300

    comment-based fix #1

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -2,7 +2,6 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.io.File;
 
@@ -11,30 +10,31 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTests {
 
     @BeforeAll
-    static void cofigureWebdriverForTests(){
-    Configuration.browserSize="1980x1020";
-    Configuration.pageLoadStrategy = "eager";
+    static void cofigureWebdriverForTests() {
+        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.browserSize = "1980x1020";
+        Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void successfulSearchTest() {
-        open("https://demoqa.com/automation-practice-form");
+        open(Configuration.baseUrl);
         $("#firstName").setValue("Виктор");
         $("#lastName").setValue("Степаныч");
-        $("#userEmail").setValue("ВикторСтепаныч@mail.ru");
+        $("#userEmail").setValue("vs@mail.ru");
         $("label[for=gender-radio-1]").click();
-        $("#userNumber").setValue("+79131234567");
+        $("#userNumber").setValue("0123456789");
         $("#dateOfBirthInput").shouldBe(Condition.visible).click();
         $$("[role='option']").filter(Condition.visible).first().click();
         $("#subjectsContainer input").setValue("12341241");
         $("label[for=hobbies-checkbox-1]").click();
-        File file = new File("C:\\image.png");
-        System.out.println("File exists ? : " + file.exists());
-        $("#uploadPicture").uploadFile(file);
+        $("#uploadPicture").uploadFromClasspath("image.png");
         $("#currentAddress").setValue("Карла Фаберже");
         $("#state").click();
         $("#react-select-3-option-0").click();
         $("#city").click();
         $("#react-select-4-option-1").click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldBe(Condition.innerText("Thanks for submitting the form"));
     }
 }
